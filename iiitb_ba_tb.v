e6691828d2235770fe06b81ca5bf130e391a1639
iiitbbcdadder <110079800+iiitbbcdadder@users.noreply.github.com>
2022-07-27 17:19:19 +0530

    Update iiitb_ba_tb.v

--- iiitb_ba_tb.v
+++ iiitb_ba_tb.v
@@ -1,31 +1,68 @@
-module iiitb_ba_tb.v;
+ module iiitb_ba_tb;
+
+
 
     // Inputs
+
     reg [3:0] a;
+
     reg [3:0] b;
+
     reg carry_in;
 
+
+
     // Outputs
+
     wire [3:0] sum;
+
     wire carry;
 
+
+
     // Instantiate the Unit Under Test (UUT)
-    bcd_adder uut (
+
+    iiitb_ba uut (
+
         .a(a), 
+
         .b(b), 
+
         .carry_in(carry_in), 
+
         .sum(sum), 
+
         .carry(carry)
+
     );
 
+
+
     initial begin
+
+    $dumpfile ("bcdadd.vcd"); 
+
+	$dumpvars(0, iiitb_ba_tb);
+
         // Apply Inputs
+
         a = 0;  b = 0;  carry_in = 0;   #100;
+
         a = 6;  b = 9;  carry_in = 0;   #100;
+
         a = 3;  b = 3;  carry_in = 1;   #100;
+
         a = 4;  b = 5;  carry_in = 0;   #100;
+
         a = 8;  b = 2;  carry_in = 0;   #100;
+
         a = 9;  b = 9;  carry_in = 1;   #100;
+
     end
+
       
+
 endmodule
+
+
+
