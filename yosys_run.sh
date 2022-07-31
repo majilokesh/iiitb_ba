63221ed58a2e04087a208b6cf0bd8e25629da6c1
iiitbbcdadder <110079800+iiitbbcdadder@users.noreply.github.com>
2022-07-31 21:36:13 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,12 +1,12 @@
 read_verilog iiitb_ba.v
 
 # generic synthesis
-synth -top bcd
+synth -top iiitb_ba
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/nisha.05.rajput/Desktop/iiitb_ba.lib
-abc -liberty /home/nisha.05.rajput/Desktop/iiitb_ba.lib
+dfflibmap -liberty /home/nisha/ASIC/lib/sky130_fd_sc_hd_tt_025c_1v80.lib
+abc -liberty /home/nisha/ASIC/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
-write_verilog -noattr synth_bcd.v
+write_verilog -noattr iiitb_ba_synth.v
